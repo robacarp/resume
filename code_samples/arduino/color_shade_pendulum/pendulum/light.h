26f64801b2c433f30ea0ab8152baefb38dd6732e
Robert L. Carpenter <robacarp@gmail.com>
2016-02-09 04:43:48 -0700

    render 2016-02-09 4:43am

--- code_samples/arduino/color_shade_pendulum/pendulum/light.h
+++
@@ -1,9 +0,0 @@
-#include "color.h"
-#include "Arduino.h"
-
-//this abstraction may now be superfluous, but ideally lights 
-//  also have a position and vector
-class Light {
-  public:
-    Color color;
-};
--- code_samples/arduino/color_shade_pendulum/pendulum/red_blink.ino
+++
@@ -1,4 +0,0 @@
-void red_blink(Light * light, int position, unsigned long tick){
-  int bright = tick % 100 > 50 ? 100 : 0;
-  light->color.set_hsb( 1, 100, bright);
-}
